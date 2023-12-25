5f9c8783a5fe51bc7ea827d0a2b3390fb7330377
Mohammad Yama Azimi <m.yamaazimi@gmail.com>
2023-12-25 04:47:33 +0430

    project setupn

---
+++ test/EbrCoin.t.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {EbrCoin} from "../src/EbrCoin.sol";
+
+contract EbrCoinTest is Test {
+    EbrCoin public ebrCoin;
+
+    // function setUp() public {
+    //     ebrCoin = new EbrCoin();
+    //     ebrCoin.setNumber(0);
+    // }
+
+    // function test_Increment() public {
+    //     ebrCoin.increment();
+    //     assertEq(ebrCoin.number(), 1);
+    // }
+}
